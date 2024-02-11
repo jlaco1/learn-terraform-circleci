496c6ddcae4258f28ec0e7f9df3df515436c914a
Jamey Lanzalaco <jlaco1@gmail.com>
2024-02-10 19:13:11 -0500

    update

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "random_uuid" "randomid" {}
 
 resource "aws_s3_bucket" "app" {
   tags = {
-    Name          = "App Bucket"
+    Name          = "JML Test App Bucket"
     public_bucket = true
   }
 
@@ -20,48 +20,48 @@ resource "aws_s3_bucket" "app" {
   force_destroy = true
 }
 
-resource "aws_s3_bucket_ownership_controls" "app" {
-  bucket = aws_s3_bucket.app.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
+# resource "aws_s3_bucket_ownership_controls" "app" {
+#   bucket = aws_s3_bucket.app.id
+#   rule {
+#     object_ownership = "BucketOwnerPreferred"
+#   }
+# }
 
-resource "aws_s3_bucket_public_access_block" "app" {
-  bucket = aws_s3_bucket.app.id
+# resource "aws_s3_bucket_public_access_block" "app" {
+#   bucket = aws_s3_bucket.app.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
+#   block_public_acls       = false
+#   block_public_policy     = false
+#   ignore_public_acls      = false
+#   restrict_public_buckets = false
+# }
 
-resource "aws_s3_bucket_acl" "app" {
-  depends_on = [
-    aws_s3_bucket_ownership_controls.app,
-    aws_s3_bucket_public_access_block.app,
-  ]
+# resource "aws_s3_bucket_acl" "app" {
+#   depends_on = [
+#     aws_s3_bucket_ownership_controls.app,
+#     aws_s3_bucket_public_access_block.app,
+#   ]
 
-  bucket = aws_s3_bucket.app.id
-  acl    = "public-read"
-}
+#   bucket = aws_s3_bucket.app.id
+#   acl    = "public-read"
+# }
 
-resource "aws_s3_bucket_website_configuration" "app" {
-  bucket = aws_s3_bucket.app.bucket
+# resource "aws_s3_bucket_website_configuration" "app" {
+#   bucket = aws_s3_bucket.app.bucket
 
-  index_document {
-    suffix = "index.html"
-  }
+#   index_document {
+#     suffix = "index.html"
+#   }
 
-  error_document {
-    key = "error.html"
-  }
-}
+#   error_document {
+#     key = "error.html"
+#   }
+# }
 
-resource "aws_s3_object" "app" {
-  acl          = "public-read"
-  key          = "index.html"
-  bucket       = aws_s3_bucket.app.id
-  content      = file("./assets/index.html")
-  content_type = "text/html"
-}
+# resource "aws_s3_object" "app" {
+#   acl          = "public-read"
+#   key          = "index.html"
+#   bucket       = aws_s3_bucket.app.id
+#   content      = file("./assets/index.html")
+#   content_type = "text/html"
+# }
